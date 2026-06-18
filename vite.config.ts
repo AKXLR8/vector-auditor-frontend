0f632251a492c40149c9c8b5987bf100d83c3e89
AkshayM <akshaymaniyampara40@gmail.com>
2026-06-18 23:43:44 +0530

    ui changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: "127.0.0.1",
     port: 5173,
+  },
+  build: {
+    rollupOptions: {
+      external: ["/config.js"],
+    },
   },
 });
